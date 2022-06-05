35b567cb58d58cdb2800eb90e8030c83e875bdfc
kellynichols99 <kellynichols99@gmail.com>
2022-06-05 15:56:15 -0500

    Legend Correction

--- Simple_Map/static/js/challenge_logic.js
+++ Simple_Map/static/js/challenge_logic.js
@@ -179,34 +179,32 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   // Here we create a legend control object.
   let legend = L.control({
     position: "bottomright"
-  
   });
+
 // Then add all the details for the legend
 legend.onAdd = function() {
   let div = L.DomUtil.create("div", "info legend");
-
   const magnitudes = [0, 1, 2, 3, 4, 5];
-  const colors = [
-    "#98ee00",
-    "#d4ee00",
-    "#eecc00",
-    "#ee9c00",
-    "#ea822c",
-    "#ea2c2c"
-  ];
+const colors = [
+  "#98ee00",
+  "#d4ee00",
+  "#eecc00",
+  "#ee9c00",
+  "#ea822c",
+  "#ea2c2c"
+];
 
 // Looping through our intervals to generate a label with a colored square for each interval.
-  for (var i = 0; i < magnitudes.length; i++) {
-    console.log(colors[i]);
-    div.innerHTML +=
-      "<i style='background: " + colors[i] + "'></i> " +
-      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-    }
-    return div;
-  };
+for (var i = 0; i < magnitudes.length; i++) {
+  console.log(colors[i]);
+  div.innerHTML +=
+    "<i style='background: " + colors[i] + "'></i> " +
+    magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+}
+return div;
+};
 
-  // Finally, we our legend to the map.
-  legend.addTo(map);
+legend.addTo(map);
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
@@ -216,7 +214,6 @@ legend.onAdd = function() {
       style: {
         color: "green",
         weight: 4
-      
       }
       
     }).addTo(techPlates);
